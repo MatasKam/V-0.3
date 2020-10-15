f0a8d46cb6c25e2cabd3cb3cafbf3f0dbf45e7d7
MatasKam <71984656+MatasKam@users.noreply.github.com>
2020-10-15 13:52:58 +0300

    Create duomenys.h

---
+++ duomenys.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "biblioteka.h"
+
+struct duomenys {
+	string vardas = "";
+	string pavarde = "";
+	float med = 0;
+	int egzp;
+	vector<int>ndp;
+
+	string var;
+	string pav;
+	vector<string> kiek;
+	string q;
+	int ilgis;
+	vector < duomenys> gaidz;
+};
